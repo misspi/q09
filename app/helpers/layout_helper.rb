43c2f7bb118700deecd355b5735694cf2e2ad897
dani <danigb@gmail.com>
2009-09-12 18:47:36 +0200

    strict mode

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -3,6 +3,6 @@ module LayoutHelper
   def cuestionario_nav(name)
     path = cuestionario_path(name)
     label = t("layouts.basic.#{name}")
-    current_page?(path) ? "<span class="">#{label}</span>" : link_to(label, path)
+    current_page?(path) ? "<span>#{label}</span>" : link_to(label, path)
   end
 end
